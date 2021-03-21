66ddd0836661e019a61ac374474b8e590d058780
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-03-20 20:01:58 -0600

    Finished and added hard solution, updated README file

--- README.md
+++ README.md
@@ -9,5 +9,11 @@ change and the total volume of the stocks. Cell formatting,
 for loops, conditionals and working with multiple worksheets
 are some skills needed to the completion of this assignment.
 
+**hard solution**
+Added bonus of obtaining the greatest increase and decrease in
+percent change in the year, plus the greatest total volume of 
+stocks,  by using the same tools. It is integrated to work
+through the whole document with one run.
+
 **resource folder**
 Contains the files used to test and run the macros.
--- challenge_hardsolution.vb
+++ challenge_hardsolution.vb
@@ -1,9 +1,9 @@
-'  Homework #2: VBA Challenge
+'  Homework #2: VBA Challenge plus Bonus
 '
 ' This code condenses the information from real stock data
-' in order to analyze it. This script contains the basic solution.
+' in order to analyze it. This script contains the hard solution.
 '
-Sub EasyChallenge()
+Sub HardChallenge()
 
      ' Do everything in every worksheet
     For Each ws In Worksheets
@@ -33,7 +33,7 @@ Sub EasyChallenge()
         Dim OpenC As Variant
         OpenC = ws.Cells(2, 3).Value
         Dim CloseC As Variant
-        Dim PercentC As Variant
+        Dim PercentC As Double
         Dim TotalV As LongLong
         TotalV = 0
 
@@ -75,6 +75,73 @@ Sub EasyChallenge()
                 TotalV = 0
             End If
         Next i
+        
+        ' Bonus exercises for the hard version
+        ' Write headers and titles for new table
+        ws.Cells(2, 14).Value = "Greatest % Increase"
+        ws.Cells(3, 14).Value = "Greatest % Decrease"
+        ws.Cells(4, 14).Value = "Greatest Total Volume"
+        ws.Cells(1, 15).Value = "Ticker"
+        ws.Cells(1, 16).Value = "Value"
+        
+        ' Format new table
+        For k = 2 To 4
+            ws.Cells(k, 14).Font.Bold = True
+            ws.Cells(k, 14).ColumnWidth = 15
+        Next k
+        For m = 15 To 16
+            ws.Cells(1, m).Font.Bold = True
+            ws.Cells(1, m).ColumnWidth = 15
+        Next m
+        
+        ' Define variables
+        Dim GInc As Double
+        Dim GDec As Double
+        Dim GTotal As LongLong
+        Dim BTickerI As String
+        Dim BTickerD As String
+        Dim BTickerT As String
+        
+        ' Find last row
+        BLastRow = ws.Cells(Rows.Count, 9).End(xlUp).Row
+        
+        ' Define starting values
+        GInc = ws.Cells(2, 11).Value
+        GDec = ws.Cells(2, 11).Value
+        GTotal = ws.Cells(2, 12).Value
+        BTickerI = ws.Cells(2, 9).Value
+        BTickerD = ws.Cells(2, 9).Value
+        BTickerT = ws.Cells(2, 9).Value
+        
+        ' For loop to find table values
+        For n = 2 To BLastRow
+            'Find greatest change
+            If GInc < ws.Cells(n, 11).Value Then
+                GInc = ws.Cells(n, 11).Value
+                BTickerI = ws.Cells(n, 9).Value
+            End If
+            'Find smallest change
+            If GDec > ws.Cells(n, 11).Value Then
+                GDec = ws.Cells(n, 11).Value
+                BTickerD = ws.Cells(n, 9).Value
+            End If
+            'Find greatest value
+            If GTotal < ws.Cells(n, 12).Value Then
+                GTotal = ws.Cells(n, 12).Value
+                BTickerT = ws.Cells(n, 9).Value
+            End If
+        Next n
+        
+        ' Put values in table
+        ws.Cells(2, 15) = BTickerI
+        ws.Cells(2, 16) = GInc
+        ws.Cells(3, 15) = BTickerD
+        ws.Cells(3, 16) = GDec
+        ws.Cells(4, 15) = BTickerT
+        ws.Cells(4, 16) = GTotal
+        ws.Range("P2,P3").NumberFormat = "0.00%"
+        
     Next ws
 
 End Sub
+
